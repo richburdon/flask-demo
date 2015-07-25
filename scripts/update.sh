0dfc249a638c2f99bdee142b911e575393177979
Rich Burdon <rich.burdon@gmail.com>
2015-07-24 22:47:38 -0400

    ok

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,13 @@ set -x
 
 brew update
 brew upgrade
+
 npm install
 npm update
 bower update
+
+pip install virtualenv
+pip install --upgrade virtualenv
+virtualenv tools/python
+tools/python/bin/pip install --upgrade -r requirements.txt
 
